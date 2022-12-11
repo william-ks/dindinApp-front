0e360b1dfadbb7a7c7b3bca95a0ed65eaba9024b
William Krisley <will.ks2101@gmail.com>
2022-12-10 22:38:13 -0300

    fix: novo link api

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: "dindinapp-back.up.railway.app",
+  baseURL: "https://dindinapp-back.up.railway.app",
   timeout: 10000,
   headers: { "X-Custom-Header": "foobar" },
 });
